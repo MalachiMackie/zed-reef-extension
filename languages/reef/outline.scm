5e0a00aded05518cbb657c4c90df4b976606f876
Malachi Mackie <malachi@mackie.co.nz>
2026-06-06 20:59:00 +1200

    Outline

---
+++ languages/reef/outline.scm
@@ -0,0 +1,20 @@
+(class_definition
+    name: (identifier) @name) @item
+
+(union_definition
+    name: (identifier) @name) @item
+
+; (member_list
+;     (_ name: (identifier) @name) @item)
+
+(class_variant name: (identifier) @name) @item
+(unit_variant name: (identifier) @name) @item
+(tuple_variant name: (identifier) @name) @item
+
+(field name: (identifier) @name) @item
+
+(function_definition
+    name: (identifier) @name) @item
+
+(attribute_definition
+    name: (identifier) @name) @item
