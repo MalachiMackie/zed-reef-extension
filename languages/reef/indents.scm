f54aecd75ca0e76e3359eabf870536c640c940b3
Malachi Mackie <malachi@mackie.co.nz>
2026-06-06 21:10:55 +1200

    Indents?

---
+++ languages/reef/indents.scm
@@ -0,0 +1,15 @@
+(object_initializer "}" @end) @indent
+
+(collection "]" @end) @indent
+
+(collection_fill "]" @end) @indent
+
+(match "}" @end) @indent
+
+(block "}" @end) @indent
+
+(class_pattern "}" @end) @indent
+
+(union_class_variant_pattern "}" @end) @indent
+
+(member_list "}" @end) @indent
